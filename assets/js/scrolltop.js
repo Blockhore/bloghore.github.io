06bbab93481e69a9e5e4f4ce05d772ff9734214f
Blockhore <blockhore.team@gmail.com>
2025-07-26 17:59:12 +0700

    Delete assets/js/scrolltop.js

--- assets/js/scrolltop.js
+++
@@ -1,7 +0,0 @@
-// === 4. SCROLL TO TOP (Optional) ===
-const toTop = document.getElementById("to-top");
-if (toTop) {
-  toTop.addEventListener("click", () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
-}
