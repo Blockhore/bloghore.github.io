8d14b4e34ffdac4d23e4d19d5f58425835e64bb2
Blockhore <blockhore.team@gmail.com>
2025-07-26 17:59:23 +0700

    Delete assets/js/navbar-toggle.js

--- assets/js/navbar-toggle.js
+++
@@ -1,9 +0,0 @@
-// === 3. MOBILE NAV TOGGLE ===
-const navToggle = document.getElementById("nav-toggle");
-const navMenu = document.getElementById("nav-menu");
-
-if (navToggle && navMenu) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-  });
-}
